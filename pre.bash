81e1278c23bdf885502235d90389c45845c9d793
Hanno Rein <hanno@hanno-rein.de>
2016-12-05 09:53:04 -0500

    done

--- pre.bash
+++ pre.bash
@@ -10,5 +10,6 @@ curl -v -O -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/uofth
 curl -v -O -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/uofthr/physicslab/master/problem.c$RANDOM"
 curl -v -O -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/uofthr/physicslab/master/restart_0051.bin?$RANDOM"
 make
+echo "DONE"
 touch /tmp/done.tag
 
