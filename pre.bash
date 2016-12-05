2896586d64988c62a6bbd0318744d441040d645e
Hanno Rein <hanno@hanno-rein.de>
2016-12-05 16:08:33 -0500

    no v

--- pre.bash
+++ pre.bash
@@ -7,9 +7,9 @@ cd problems
 mkdir ss_lab1
 cd ss_lab1
 LASTCOMMIT=`curl -s https://api.github.com/repos/uofthr/ss_lab1/commits/master | grep sha |head -n 1 | cut -d\" -f4`
-curl -v -O -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/uofthr/ss_lab1/$LASTCOMMIT/Makefile"
-curl -v -O -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/uofthr/ss_lab1/$LASTCOMMIT/problem.c"
-curl -v -O -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/uofthr/ss_lab1/$LASTCOMMIT/restart_0051.bin"
-curl -v -O -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/uofthr/ss_lab1/$LASTCOMMIT/run.bash"
+curl -O -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/uofthr/ss_lab1/$LASTCOMMIT/Makefile"
+curl -O -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/uofthr/ss_lab1/$LASTCOMMIT/problem.c"
+curl -O -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/uofthr/ss_lab1/$LASTCOMMIT/restart_0051.bin"
+curl -O -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/uofthr/ss_lab1/$LASTCOMMIT/run.bash"
 chmod a+x run.bash
 make
