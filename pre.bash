79588b782ab8568c3520cd91488acf2559841787
Hanno Rein <hanno@hanno-rein.de>
2016-12-05 10:07:35 -0500

    moving everything here

--- pre.bash
+++ pre.bash
@@ -6,9 +6,9 @@ mkdir problems
 cd problems
 mkdir ss_lab1
 cd ss_lab1
-curl -v -O -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/uofthr/physicslab/master/Makefile?$RANDOM"
-curl -v -O -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/uofthr/physicslab/master/problem.c$RANDOM"
-curl -v -O -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/uofthr/physicslab/master/restart_0051.bin?$RANDOM"
+curl -v -O -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/uofthr/physicslab/0a25007/Makefile"
+curl -v -O -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/uofthr/physicslab/0a25007/problem.c"
+curl -v -O -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/uofthr/physicslab/0a25007/restart_0051.bin"
 make
 echo "DONE3"
 touch /tmp/done.tag
