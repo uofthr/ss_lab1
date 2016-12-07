ae8f4eabd19e053ad6665715711b0bb14bd41c33
Hanno Rein <hanno@hanno-rein.de>
2016-12-07 09:37:35 -0500

    new download

--- pre.bash
+++ pre.bash
@@ -1,15 +1,18 @@
 #!/bin/bash
 cd /tmp
-git clone https://github.com/hannorein/rebound
+curl -s -S -O -H 'Cache-Control: no-cache' "https://codeload.github.com/hannorein/rebound/zip/master"
+unzip master
+rm -f master
+mv rebound-master rebound
 cd rebound
 mkdir problems
 cd problems
 mkdir ss_lab1
 cd ss_lab1
 LASTCOMMIT=`curl -s https://api.github.com/repos/uofthr/ss_lab1/commits/master | grep sha |head -n 1 | cut -d\" -f4`
-curl -O -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/uofthr/ss_lab1/$LASTCOMMIT/Makefile"
-curl -O -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/uofthr/ss_lab1/$LASTCOMMIT/problem.c"
-curl -O -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/uofthr/ss_lab1/$LASTCOMMIT/restart_0051.bin"
-curl -O -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/uofthr/ss_lab1/$LASTCOMMIT/run.bash"
+curl -s -S -O -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/uofthr/ss_lab1/$LASTCOMMIT/Makefile"
+curl -s -S -O -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/uofthr/ss_lab1/$LASTCOMMIT/problem.c"
+curl -s -S -O -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/uofthr/ss_lab1/$LASTCOMMIT/restart_0051.bin"
+curl -s -S -O -H 'Cache-Control: no-cache' "https://raw.githubusercontent.com/uofthr/ss_lab1/$LASTCOMMIT/run.bash"
 chmod a+x run.bash
 make
